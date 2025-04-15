3c6f3ad4038df653816efe2c818c64788a3bed8c
tyna <tinhinane.ziane2002@gmail.com>
2025-04-15 14:37:27 +0200

    Initialisation du projet : structure + fichiers de base

---
+++ backend/etudiants-services/etudiants-services/src/test/java/com/etudiants/etudiants_services/EtudiantsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etudiants.etudiants_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtudiantsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
